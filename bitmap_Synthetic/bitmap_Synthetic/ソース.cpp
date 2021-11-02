4b67d0f9c7763f45992d6f81d6afee1ba6a6c7e2
kiru193 <program922@hotmail.com>
2021-11-02 17:51:53 +0900

    ビットマップの追加 前にもあった横長になるバグ？みたいなのまだあるんだね．

--- bitmap_Synthetic/bitmap_Synthetic/ソース.cpp
+++ bitmap_Synthetic/bitmap_Synthetic/ソース.cpp
@@ -4,7 +4,35 @@ using namespace cv;
 
 int main()
 {
-    Mat image = Mat::zeros(100, 100, CV_8UC3);
-    imshow("", image);
+    
+    Mat image = imread("sample.bmp");
+    Mat correction_image = imread("correction.bmp");
+    int col = image.cols;
+    int row = image.rows;
+    Mat create_image = Mat::zeros(row, col, CV_8U);
+    printf("image col %d row %d\n", col, row);
+    printf("create image col %d row %d", create_image.cols, create_image.rows);
+
+    if (image.empty()||correction_image.empty() == true) {
+        printf("nothing");
+        return 0;
+    }
+
+    for (int i = 0; i < row; i++) {
+        for (int m = 0; m < col; m++) {
+            //create_image.at<unsigned char>(i, m) = correction_image.at<unsigned char>(i, m);
+            //printf("%d %d\n", m, i);
+            
+            if (correction_image.at<unsigned char>(i, m) + image.at<unsigned char>(i, m) <= 255) {
+                create_image.at<unsigned char>(i, m) = correction_image.at<unsigned char>(i, m) + image.at<unsigned char>(i, m);
+            }
+            else {
+                create_image.at<unsigned char>(i, m) = correction_image.at<unsigned char>(i, m) + image.at<unsigned char>(i, m)-255;
+            }
+            
+        }
+    }
+    
+    imshow("sample.bmp", create_image);
     waitKey(0);
 }
