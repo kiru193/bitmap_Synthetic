3d56a7e475f77f56ff7014ade747b41f3fac0c8e
kiru193 <program922@hotmail.com>
2021-11-03 17:24:42 +0900

    ・横長になる原因 元々作られていたbitmapはモノクロの情報ではなくて3色の情報を持ってるタイプのデータ構造をしていたと考えられる． 移す先のデータ構造をモノクロのタイプのデータ構造であったためＲＧＢの情報が横に広がる形となって表れてしまった． ・opencvのデータアクセスについて 今までatを用いたデータアクセスをしていたが，連続アクセス（今回のように一つ一つの画素を確認して足し合わせるなど）の場合，dataの方が速いらしい． 覚え書き．

--- bitmap_Synthetic/bitmap_Synthetic/ソース.cpp
+++ bitmap_Synthetic/bitmap_Synthetic/ソース.cpp
@@ -1,38 +1,46 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
 
 int main()
 {
-    
     Mat image = imread("sample.bmp");
     Mat correction_image = imread("correction.bmp");
     int col = image.cols;
     int row = image.rows;
-    Mat create_image = Mat::zeros(row, col, CV_8U);
+    Mat create_image = Mat::zeros(row, col, CV_8UC3);
+    string name;
+
     printf("image col %d row %d\n", col, row);
-    printf("create image col %d row %d", create_image.cols, create_image.rows);
+    printf("create image col %d row %d\n", create_image.cols, create_image.rows);
 
     if (image.empty()||correction_image.empty() == true) {
-        printf("nothing");
+        printf("nothing\n");
         return 0;
     }
 
-    for (int i = 0; i < row; i++) {
+    //printf("%d %d %d\n", correction_image.data[row*col+1]);
+    for (int i = 0; i < 3*row; i++) {
         for (int m = 0; m < col; m++) {
-            //create_image.at<unsigned char>(i, m) = correction_image.at<unsigned char>(i, m);
-            //printf("%d %d\n", m, i);
+            //create_image.data[i * col + m] = correction_image.data[i * col + m];
+            //printf("%d %d %d\n", m, i, create_image.data[i * row + m]);
             
-            if (correction_image.at<unsigned char>(i, m) + image.at<unsigned char>(i, m) <= 255) {
-                create_image.at<unsigned char>(i, m) = correction_image.at<unsigned char>(i, m) + image.at<unsigned char>(i, m);
+            if (correction_image.data[i * row + m] + image.data[i * row + m] <= 255) {
+                create_image.data[i * col + m] = correction_image.data[i * col + m] + image.data[i * col + m];
             }
             else {
-                create_image.at<unsigned char>(i, m) = correction_image.at<unsigned char>(i, m) + image.at<unsigned char>(i, m)-255;
-            }
+                create_image.data[i * col + m] = correction_image.data[i * col + m] + image.data[i * col + m] - 255;
+            } 
             
         }
     }
     
     imshow("sample.bmp", create_image);
     waitKey(0);
+    destroyAllWindows();
+
+    cout << "please input this picture name\n";
+    cin >> name;
+    imwrite(name + ".bmp", create_image);
 }
